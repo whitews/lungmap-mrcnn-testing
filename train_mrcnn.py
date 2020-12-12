bdfb95ee7b126751b84fd44fcfbb7e04653bc1a9
whitews <whitews@gmail.com>
2020-12-11 21:59:56 -0500

    update weights_path in train_mrcnn.py

--- train_mrcnn.py
+++ train_mrcnn.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 tf.config.set_visible_devices([], 'GPU')
 
 image_set_dir = '../if-microscopy-analysis-pipeline/examples/mouse_E16.5/images'
-weights_path = "tmp/mask_rcnn_lungmap_0001.h5"
+weights_path = "mask_rcnn_lungmap_last.h5"  # change to 'coco' to start from scratch
 
 val_img_name = '2015-04-029_20X_C57Bl6_E16.5_LMM.14.24.4.46_SOX9_SFTPC_ACTA2_002.tif'
 test_img_name = '2015-04-029_20X_C57Bl6_E16.5_LMM.14.24.4.46_SOX9_SFTPC_ACTA2_001.tif'
